8f7f6d7e9c3f8c464deb2884af3f7f19e8f50472
Sergio Neves <serneves@hotmail.com>
2018-11-11 16:28:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function setCart(c) {
 
 function addToCart(item) {
  cart.push({itemName: item}, {itemPrice: Math.random()*10})
- return item + ` has been added to your cart.`
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
