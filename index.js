a750edcf5226ac790c0f9a56845a8a046dbee516
Sergio Neves <serneves@hotmail.com>
2018-11-11 16:33:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var cart = [];
-
-//function getRandomInt(max) {
-  //return Math.floor(Math.random() * Math.floor(max))
+var cart = [{ itemName: name of the item}, {itemPrice: price of the item, }];
 
 function getCart() {
  return cart;
@@ -14,6 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  cart.push({itemName: item}, {itemPrice: Math.random()*10})
+
  return `${item} has been added to your cart.`
 }
 
