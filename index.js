6c759a83470cb63bb7e22078376757792f65c742
Sergio Neves <serneves@hotmail.com>
2018-11-11 16:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max))
+//function getRandomInt(max) {
+  //return Math.floor(Math.random() * Math.floor(max))
 
 function getCart() {
  return cart;
